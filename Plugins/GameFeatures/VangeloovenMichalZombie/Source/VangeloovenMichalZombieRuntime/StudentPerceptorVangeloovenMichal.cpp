18e9f90e30bd196dd6b1465e951396c11a33948c
MichalVGL <michal.vangelooven@student.howest.be>
2026-06-02 23:47:28 +0200

    Added start files

---
+++ Plugins/GameFeatures/VangeloovenMichalZombie/Source/VangeloovenMichalZombieRuntime/StudentPerceptorVangeloovenMichal.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StudentPerceptorVangeloovenMichal.h"
+
+#include "AIController.h"
+#include "Survivor/SurvivorBlackboardVangeloovenMichal.h"
+
+
+UStudentPerceptorVangeloovenMichal::UStudentPerceptorVangeloovenMichal()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UStudentPerceptorVangeloovenMichal::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (auto PerceptionComp = GetOwner()->GetComponentByClass<UAIPerceptionComponent>())
+	{
+		PerceptionComp->OnTargetPerceptionUpdated.AddDynamic(this, &UStudentPerceptorVangeloovenMichal::OnPerceptionUpdated);
+	}
+}
+
+void UStudentPerceptorVangeloovenMichal::OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
+	GEngine->AddOnScreenDebugMessage(5, 1.f, FColor::Green, 
+	FString::Printf(TEXT("Saw Something!")));
+	
+	APawn* Pawn = Cast<APawn>(GetOwner());
+	if (!Pawn) return;
+
+	AAIController* AIController = Cast<AAIController>(Pawn->GetController());
+	if (AIController)
+	{
+		SurvivorBlackboardVangeloovenMichal bb{AIController->GetBlackboardComponent()};
+		
+		bb.SetPickup(Actor);\
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(5, 1.f, FColor::Green, 
+	FString::Printf(TEXT("No AIController")));
+	}
+}
---
+++ Plugins/GameFeatures/VangeloovenMichalZombie/Source/VangeloovenMichalZombieRuntime/Survivor/SurvivorBlackboardVangeloovenMichal.h
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "UObject/Object.h"
+
+namespace my_namespace
+{
+	
+}
+
+namespace SurvivorBBItemsVangeloovenMichal
+{
+	inline const FName Actor{"SelfActor"};
+	inline const FName Pickup{"Pickup"};
+}
+
+struct SurvivorBlackboardVangeloovenMichal
+{
+	SurvivorBlackboardVangeloovenMichal(UBlackboardComponent* blackboard)
+			: Blackboard{blackboard}
+	{
+	}
+	
+	UBlackboardComponent* Blackboard;
+	
+	// --- SelfActor ---
+	AActor* GetActor() const
+	{
+		return Cast<AActor>(Blackboard->GetValueAsObject(SurvivorBBItemsVangeloovenMichal::Actor));
+	}
+
+	void SetActor(AActor* Actor) const
+	{
+		Blackboard->SetValueAsObject(SurvivorBBItemsVangeloovenMichal::Actor, Actor);
+	}
+	
+	// --- Pickup ---
+	AActor* GetPickup() const
+	{
+		return Cast<AActor>(Blackboard->GetValueAsObject(SurvivorBBItemsVangeloovenMichal::Pickup));
+	}
+
+	void SetPickup(AActor* Actor) const
+	{
+		Blackboard->SetValueAsObject(SurvivorBBItemsVangeloovenMichal::Pickup, Actor);
+	}
+};
