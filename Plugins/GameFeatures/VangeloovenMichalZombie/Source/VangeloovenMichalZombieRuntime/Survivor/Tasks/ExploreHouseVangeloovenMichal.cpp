983ea016e8d149213f3f6f2c794cddc9a580fe50
MichalVGL <michal.vangelooven@student.howest.be>
2026-06-06 12:29:56 +0200

    Added missing files

---
+++ Plugins/GameFeatures/VangeloovenMichalZombie/Source/VangeloovenMichalZombieRuntime/Survivor/Tasks/ExploreHouseVangeloovenMichal.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ExploreHouseVangeloovenMichal.h"
+
+#include "VangeloovenMichalZombieRuntime/Survivor/SurvivorBlackboardVangeloovenMichal.h"
+#include "VangeloovenMichalZombieRuntime/Utils/LoggerVangeloovenMichal.h"
+
+UExploreHouseVangeloovenMichal::UExploreHouseVangeloovenMichal()
+{
+	NodeName = "ExploreHouse";
+}
+
+EBTNodeResult::Type UExploreHouseVangeloovenMichal::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	SurvivorBlackboardVangeloovenMichal bb{OwnerComp.GetBlackboardComponent()};
+	
+	if (!bb.Blackboard)
+		return EBTNodeResult::Failed;
+	
+	AHouse* House = Cast<AHouse>(bb.GetCurrentTargetObject());
+	
+	if (!House)
+	{
+		Logger::Console(TEXT("[ExploreHouse] Cast failed, CurrentTargetObject is not AHouse"));
+		return EBTNodeResult::Failed;
+	}
+	
+	auto* SeenHouses = bb.GetSeenHouses();
+	if (!SeenHouses)
+		return EBTNodeResult::Failed;
+	
+	auto* ClearedHouses = bb.GetClearedHouses();
+	if (!ClearedHouses)
+		return EBTNodeResult::Failed;
+	
+	std::erase(SeenHouses->Houses, House);
+	ClearedHouses->Houses.push_back(House);
+	bb.SetCurrentTargetObject(nullptr);
+	
+	return EBTNodeResult::Succeeded;
+}
+
+
---
+++ Plugins/GameFeatures/VangeloovenMichalZombie/Source/VangeloovenMichalZombieRuntime/Survivor/Tasks/ExploreHouseVangeloovenMichal.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "ExploreHouseVangeloovenMichal.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VANGELOOVENMICHALZOMBIERUNTIME_API UExploreHouseVangeloovenMichal : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+public: 
+	UExploreHouseVangeloovenMichal();
+	
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
